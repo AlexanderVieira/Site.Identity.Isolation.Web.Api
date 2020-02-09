a40bebe03ab51ff934f30b34f56a23d233f48704
Alexander <Alexander@DESKTOP-8RM83T5>
2020-02-09 03:25:04 -0300

    Implementando SimpleInjectorInitializer na camada Infra, bem como na camada de aplicação (web.api).

--- src/Application/SiteIdentityIsolation.API/App_Start/Startup.cs
+++ src/Application/SiteIdentityIsolation.API/App_Start/Startup.cs
@@ -3,10 +3,10 @@ using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.Owin.Security.Facebook;
 using Owin;
-using SiteIdentityIsolation.Infra.Identity.Security.ContextIdentity;
 using SiteIdentityIsolation.Infra.Identity.Security.Identity;
 using System;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace SiteIdentityIsolation.API
 {
@@ -17,11 +17,14 @@ namespace SiteIdentityIsolation.API
         // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
         public void ConfigureAuth(IAppBuilder app)
         {
+            // Configure the db context, user manager and signin manager to use a single instance per request
+            app.CreatePerOwinContext(() => DependencyResolver.Current.GetService<ApplicationUserManager>());
+
             // Configure the db context, user manager and role manager to use a single instance per request
-            app.CreatePerOwinContext(ApplicationDbContext.Create);
-            app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
-            app.CreatePerOwinContext<ApplicationRoleManager>(ApplicationRoleManager.Create);
-            app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
+            //app.CreatePerOwinContext(ApplicationDbContext.Create);
+            //app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
+            //app.CreatePerOwinContext<ApplicationRoleManager>(ApplicationRoleManager.Create);
+            //app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
             // Enable the application to use a cookie to store information for the signed in user
             // and to use a cookie to temporarily store information about a user logging in with a third party login provider
---
+++ src/Infra/CrossCuting/IoC/SiteIdentityIsolation.Infra.CrossCuting.IoC/BootStrapper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using SimpleInjector;
+using SiteIdentityIsolation.Infra.Identity.Security.ContextIdentity;
+using SiteIdentityIsolation.Infra.Identity.Security.Identity;
+using SiteIdentityIsolation.Infra.Identity.Security.Models;
+
+namespace SiteIdentityIsolation.Infra.CrossCuting.IoC
+{
+    public class BootStrapper
+    {
+        public static void RegisterServices(Container container)
+        {
+            container.RegisterPerWebRequest<ApplicationDbContext>();
+            container.RegisterPerWebRequest<IUserStore<ApplicationUser>>(() => new UserStore<ApplicationUser>(new ApplicationDbContext()));
+            container.RegisterPerWebRequest<IRoleStore<IdentityRole, string>>(() => new RoleStore<IdentityRole>());
+            container.RegisterPerWebRequest<ApplicationRoleManager>();
+            container.RegisterPerWebRequest<ApplicationUserManager>();
+            container.RegisterPerWebRequest<ApplicationSignInManager>();
+
+            //container.RegisterPerWebRequest<IUsuarioRepository, UsuarioRepository>();
+        }
+    }
+}
